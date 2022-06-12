485cd2bf3f3b065cb9e17700cbd5060b1aa99ae2
Tyrion <tyrionliu@lilith>
2022-06-12 15:31:52 +0800

    chore: 0.0.7

---
+++ packages/core/lib/index.js
@@ -0,0 +1,9 @@
+'use strict';
+const pkg = require('../package.json')
+
+module.exports = core;
+
+function core() {
+    // TODO
+    console.log('exec ', pkg.version)
+}
--- packages/utils/lib/utils.js
+++ packages/utils/lib/utils.js
@@ -3,5 +3,5 @@
 module.exports = utils;
 
 function utils() {
-    // TODOddddd
+    // TODOddddd999
 }
